adee257f37963001de7016a0d7791f337a496599
Bharghava Varun Ayada <varun@ip-192-168-0-103.ec2.internal>
2018-03-08 11:55:46 +0530

    build script

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -2,6 +2,11 @@
 
 TAG=$1
 
+if [ -z $TAG ];then
+   echo -e "\nERROR: No docker build tag provided. Stopping build...\n"
+   exit 1
+fi
+
 docker login --username=abvarun226
 
 cp -r /etc/ssl/certs .
--- bin/push_image.sh
+++ bin/push_image.sh
@@ -2,6 +2,11 @@
 
 TAG=$1
 
+if [ -z $TAG ];then
+   echo -e "\nERROR: No docker build tag provided. Stopping push...\n"
+   exit 1
+fi
+
 docker login --username=abvarun226
 
 docker push abvarun226/heapster-grafana-amd64:${TAG}
